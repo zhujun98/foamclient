edc35b30a0f0c771d93b9f739c072e869c4e1287
Jun Zhu <zhujun981661@gmail.com>
2023-01-12 21:45:33 +0100

    Merge RedisConsumer and RedisProducer into a single file

--- foamclient/__init__.py
+++ foamclient/__init__.py
@@ -1,5 +1,4 @@
 from .deserializer import DeserializerType
-from .redis_consumer import RedisConsumer
-from .redis_producer import RedisProducer
+from .redis_clients import RedisConsumer, RedisProducer
 from .serializer import SerializerType
 from .zmq_consumer import ZmqConsumer
